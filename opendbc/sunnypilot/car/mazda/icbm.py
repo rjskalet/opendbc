c5ee50641b6c1d55487065d030b7827582b9c3ea
rjskalet <99288083+rjskalet@users.noreply.github.com>
2026-09-16 10:39:46 -0500

    mazda: use measured smart-cruise button pacing

--- opendbc/sunnypilot/car/mazda/icbm.py
+++ opendbc/sunnypilot/car/mazda/icbm.py
@@ -1,14 +1,15 @@
 """
-Copyright (c) 2021-, Haibin Wen, sunnypilot, and a number of other contributors.
+Mazda Intelligent Cruise Button Management interface.
 
-This file is part of sunnypilot and is licensed under the MIT License.
-See the LICENSE.md file in the root directory for more details.
+Keeps synthesized stock-ACC set-speed changes on the measured Mazda message cadence and
+never transmits over a physical SET+/SET- press.
 """
 
 from opendbc.car import structs, DT_CTRL
 from opendbc.car.can_definitions import CanData
 from opendbc.car.mazda import mazdacan
 from opendbc.car.mazda.values import Buttons
+from opendbc.sunnypilot.car.icbm_actuation_profile import get_actuation_profile
 from opendbc.sunnypilot.car.intelligent_cruise_button_management_interface_base import IntelligentCruiseButtonManagementInterfaceBase
 
 ButtonType = structs.CarState.ButtonEvent.Type
@@ -17,12 +18,20 @@ SendButtonState = structs.IntelligentCruiseButtonManagement.SendButtonState
 BUTTONS = {
   SendButtonState.increase: Buttons.SET_PLUS,
   SendButtonState.decrease: Buttons.SET_MINUS,
+  SendButtonState.increaseHold: Buttons.SET_PLUS,
+  SendButtonState.decreaseHold: Buttons.SET_MINUS,
 }
+HOLD_BUTTONS = (SendButtonState.increaseHold, SendButtonState.decreaseHold)
+
+# A 10 Hz hold stream mirrors CRZ_BTNS's native cadence. Interleaved with the real wheel stream,
+# the body ECU registers these as reliably paced one-unit moves rather than an unsafe burst.
+HOLD_PERIOD = 0.1
 
 
 class IntelligentCruiseButtonManagementInterface(IntelligentCruiseButtonManagementInterfaceBase):
   def __init__(self, CP, CP_SP):
     super().__init__(CP, CP_SP)
+    self.tap_period = 1. / get_actuation_profile(CP.brand).tap_rate_hz
 
   def update(self, CC_SP, CS, packer, frame, last_button_frame) -> list[CanData]:
     can_sends = []
@@ -31,14 +40,24 @@ class IntelligentCruiseButtonManagementInterface(IntelligentCruiseButtonManageme
     self.frame = frame
     self.last_button_frame = last_button_frame
 
+    # Never forge a set-speed frame over the driver's physical SET+/SET- input.
+    if CS.accel_button or CS.decel_button:
+      return can_sends
+
     if self.ICBM.sendButton != SendButtonState.none:
       send_button = BUTTONS[self.ICBM.sendButton]
+      since_last_send = (self.frame - self.last_button_frame) * DT_CTRL
 
-      if (self.frame - self.last_button_frame) * DT_CTRL > 0.2:
-        self.button_frame += 1
-        button_counter_offset = [1, 1, 0, None][self.button_frame % 4]
-        if button_counter_offset is not None:
-          can_sends.append(mazdacan.create_button_cmd(packer, self.CP, CS.crz_btns_counter + button_counter_offset, send_button))
+      if self.ICBM.sendButton in HOLD_BUTTONS:
+        if since_last_send > HOLD_PERIOD:
+          can_sends.append(mazdacan.create_button_cmd(packer, self.CP, CS.crz_btns_counter + 1, send_button))
           self.last_button_frame = self.frame
+      else:
+        if since_last_send > self.tap_period:
+          self.button_frame += 1
+          button_counter_offset = [1, 1, 0, None][self.button_frame % 4]
+          if button_counter_offset is not None:
+            can_sends.append(mazdacan.create_button_cmd(packer, self.CP, CS.crz_btns_counter + button_counter_offset, send_button))
+            self.last_button_frame = self.frame
 
     return can_sends
